418adfedc45a20cd8b5fddd67c09a711d7e484ef
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-05-24 17:26:39 -0400

    Setup router

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,23 @@
 	import Home from './pages/Home.svelte';
 	import About from './pages/About.svelte';
 
+	// router
+	import { Router, Route, Link } from 'svelte-routing';
+
 </script>
 
-<h1>Hello</h1>
+<Router>
+	<nav>
+		<Link to='/'>Home</Link>
+		<Link to='/about'>About</Link>
+		<Link to='/products'>Products</Link>
+
+	</nav>
+	<Route path='/' component={Home} />
+	<Route path='/about' component={About} />
+	<Route path='/login' component={Login} />
+	<Route path='/products' component={Products} />
+	<Route path='/products/:id' component={ProductTemplate} />
+
+</Router>
+
