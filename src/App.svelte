493bb35829131d7a88034218d50c480a864fdd25
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-05-29 16:50:40 -0400

    created a sidebar

--- src/App.svelte
+++ src/App.svelte
@@ -12,11 +12,13 @@
 
 	// components
 	import Navbar from './components/Navbar/Navbar.svelte';
+	import Sidebar from './components/Navbar/Sidebar.svelte';
+
 </script>
 
 <Router>
 	<Navbar />
-	
+	<Sidebar />
 	<Route path='/' component={Home} />
 	<Route path='/about' component={About} />
 	<Route path='/login' component={Login} />
---
+++ src/components/Navbar/Sidebar.svelte
@@ -0,0 +1,27 @@
+<script>
+    import links from '../../constants/links';
+    import { link } from 'svelte-routing';
+</script>
+
+<div class="sidebar-container">
+    <div class="sidebar">
+        <!-- header -->
+        <div class="sidebar-header">
+           <button class='btn-close'>
+               <i class="fas fa-window-close" />
+           </button> 
+        </div>
+        <!-- site logo -->
+        <img src="/assets/images/logo.svg" class='logo sidebar-logo' alt="razors logo">
+        <!-- links -->
+        <ul class="sidebar-links">
+            {#each links as sideLink}
+            <li>
+                <a href={sideLink.url} use:link on:click={() => {
+                    console.log('sidebar closed');
+                }}>{sideLink.text}</a>
+            </li>
+            {/each}
+        </ul>
+    </div>
+</div>
