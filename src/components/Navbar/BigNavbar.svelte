e4dd9bcd710e5919c46cd066d2f9b5e8174b8824
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-05-29 14:34:05 -0400

    did big navbar

---
+++ src/components/Navbar/BigNavbar.svelte
@@ -0,0 +1,21 @@
+<script>
+    import CartButton from '../Cart/CartButton.svelte';
+    import { link } from 'svelte-routing';
+</script>
+
+<nav class="navbar">
+    <div class="nav-container">
+<div class="nav-center">
+    <!-- nav links -->
+    <p>nav links</p>
+    <!-- nav logo -->
+    <a href="/" use:link class='nav-logo big-logo'>
+    <img src="/assets/images/logo.svg" class='logo' alt="razors logo">
+    </a>
+    <!-- cart toggle -->
+    <div class="nav-aside">
+        <CartButton />
+    </div>
+</div>
+</div>
+</nav>
--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -1,5 +1,8 @@
 <script>
     import SmallNavbar from './SmallNavbar.svelte';
+    import BigNavbar from './BigNavbar.svelte';
+
 </script>
 
 <SmallNavbar />
+<BigNavbar />
