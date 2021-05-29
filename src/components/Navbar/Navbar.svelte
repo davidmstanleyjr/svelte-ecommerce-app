603d42c741e4d07739ae77251986caa5014f5355
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-05-29 16:04:19 -0400

    altered the screen width. Now big navbar shows up when the screen width is above 992px

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -1,8 +1,18 @@
 <script>
     import SmallNavbar from './SmallNavbar.svelte';
     import BigNavbar from './BigNavbar.svelte';
+    let screenWidth;
+    
 
 </script>
+<!-- screenWidth logic -->
+<svelte:window bind:innerWidth={screenWidth} />
 
-<SmallNavbar />
+{#if screenWidth > 992}
+
 <BigNavbar />
+
+{:else}
+<SmallNavbar />
+
+{/if}
