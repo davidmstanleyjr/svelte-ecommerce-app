3377c38f30f48b4bf1e3041860fe5ab73540b2d3
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-05-29 23:04:23 -0400

    did more work on the globalStore

--- src/App.svelte
+++ src/App.svelte
@@ -13,12 +13,17 @@
 	// components
 	import Navbar from './components/Navbar/Navbar.svelte';
 	import Sidebar from './components/Navbar/Sidebar.svelte';
+	// global store 
+	import globalStore from './stores/globalStore';
+	
 
 </script>
 
 <Router>
 	<Navbar />
+	{#if $globalStore.sidebar}
 	<Sidebar />
+	{/if}
 	<Route path='/' component={Home} />
 	<Route path='/about' component={About} />
 	<Route path='/login' component={Login} />
