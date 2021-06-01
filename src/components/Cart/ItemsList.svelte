4e1ac12f162f72b3f5b80d556cece0999d5a75cf
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-06-01 15:18:37 -0400

    added cart transitions

--- src/components/Cart/ItemsList.svelte
+++ src/components/Cart/ItemsList.svelte
@@ -8,7 +8,10 @@ import {flip} from 'svelte/animate';
 <section class='cart-items'>
 <article>
     {#each $cart as cartItem, index (cartItem.id)}
+    <div in:fly={{ delay: (index+1) * 500, x: 100 }}
+    out:fly={{ x: -100}} animate:flip>
     <Item {...cartItem} />
+</div>
     {:else}
     <h2 class='empty-cart'>is currently empty</h2>
     {/each}
