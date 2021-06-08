9e84ca51146dc299a038fd888f863c2b5b1b7a78
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-06-07 21:32:38 -0400

    strapi api stuff

---
+++ src/strapi/URL.js
@@ -0,0 +1,2 @@
+// url for deployed project. Makes life easier so i'm not changing things around back and forth.
+export default 'http://localhost:1337'
---
+++ src/strapi/getProducts.js
@@ -0,0 +1,10 @@
+import url from './URL';
+
+export default async () => {
+    const response = await fetch(`${url}/products`).catch (error => console.error(error));
+    const products = await response.json();
+    if (products.error) {
+        return null;
+    }
+    return products;
+}
