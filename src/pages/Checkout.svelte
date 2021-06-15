05046fc4ebed31ba8aec5e369132e48e58f349fe
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-06-15 12:55:20 -0400

    added stripe token to the checkout logic

--- src/pages/Checkout.svelte
+++ src/pages/Checkout.svelte
@@ -1,100 +1,99 @@
 <script>
-    import {onMount} from 'svelte';
-    import {navigate, link} from 'svelte-routing';
-    import user from '../stores/user';
-    import { cartTotal } from '../stores/cart';
+	import { onMount } from 'svelte';
+	import { navigate, link } from 'svelte-routing';
+	import user from '../stores/user';
+	import { cartTotal } from '../stores/cart';
 
-    let name = '';
-// stripe variables 
-let cardElement;
-let cardErrors;
-let card;
-let stripe;
-let elements;
+	let name = '';
+	// stripe variables
+	let cardElement;
+	let cardErrors;
+	let card;
+	let stripe;
+	let elements;
 
-    $: isEmpty = !name;
+	$: isEmpty = !name;
 
-// doesn't allow user to go to the checkout page by typing in the URL
-onMount(() => {
-    if(!$user.jwt) {
-        navigate('/');
-        return;
-    }
-    stripe = Stripe('pk_test_51J21TSIqwC4uAJxr3n6yu3oGuXfwTdbapUeO1rZJRhSF8BQ302NDGuUO6k7nt8uhAGxYNIeHBtlE2pnx5AxjjmCj00vNPTy32S');
-    elements = stripe.elements();
-    card = elements.create('card');
-    card.mount(cardElement);
-    card.addEventListener('change', function(event) {
-        if (event.error) {
-            cardErrors.textContent = event.error.message;
+	// doesn't allow user to go to the checkout page by typing in the URL
+	onMount(() => {
+		if (!$user.jwt) {
+			navigate('/');
+			return;
+		}
+		stripe = Stripe(
+			'pk_test_51J21TSIqwC4uAJxr3n6yu3oGuXfwTdbapUeO1rZJRhSF8BQ302NDGuUO6k7nt8uhAGxYNIeHBtlE2pnx5AxjjmCj00vNPTy32S'
+		);
+		elements = stripe.elements();
+		card = elements.create('card');
+		card.mount(cardElement);
+		card.addEventListener('change', function (event) {
+			if (event.error) {
+				cardErrors.textContent = event.error.message;
+			} else {
+				cardErrors.textContent = '';
+			}
+		});
+	});
+
+	async function handleSubmit() {
+		let response = await stripe.createToken(card).catch(error => console.log(error));
+        const {token} = response;
+        if (token) {
+            console.log(response);
+            // token id
+            // submit order
+        } else {
+            console.log(response);
         }
-        else { 
-            cardErrors.textContent = '';
-        }
-    })
-});
-
-function handleSubmit() {
-    console.log('form submitted');
-}
-
-
+	}
 </script>
 
-
 {#if $cartTotal > 0}
-<section class="form">
-    <h2 class="section-title">checkout</h2>
-    <form class="checkout-form" on:submit|preventDefault={handleSubmit}>
-    <h3>order total : ${$cartTotal}</h3>
-    <!-- single input -->
-    <div class="form-control">
-        <label for="name">your name</label>
-        <input type="text" id='name' bind:value={name}>
-    </div>
-    <!-- end of single input -->
+	<section class="form">
+		<h2 class="section-title">checkout</h2>
+		<form class="checkout-form" on:submit|preventDefault={handleSubmit}>
+			<h3>order total : ${$cartTotal}</h3>
+			<!-- single input -->
+			<div class="form-control">
+				<label for="name">your name</label>
+				<input type="text" id="name" bind:value={name} />
+			</div>
+			<!-- end of single input -->
 
-    <!-- stripe stuff -->
-    <div class="stripe-input">
-    <!-- info -->
-        <label for="card-element">Credit or Debit</label>
-        <p class="stripe-info">
-            Test using this credit card: 
-            <span>4242 4242 4242 4242</span>
-            <br>
-            enter any 5 digits for the zip code
-            <br>
-            enter any 3 digits for the CVC
-        </p>
+			<!-- stripe stuff -->
+			<div class="stripe-input">
+				<!-- info -->
+				<label for="card-element">Credit or Debit</label>
+				<p class="stripe-info">
+					Test using this credit card:
+					<span>4242 4242 4242 4242</span>
+					<br />
+					enter any 5 digits for the zip code
+					<br />
+					enter any 3 digits for the CVC
+				</p>
 
+				<div id="card-element" bind:this={cardElement}>
+					<!-- stripe -->
+				</div>
+				<div id="card-errors" bind:this={cardErrors} role="alert" />
+			</div>
 
+			<!-- end of stripe stuff -->
 
-        <div id="card-element" bind:this={cardElement}>
-        <!-- stripe -->
-        </div>
-        <div id="card-errors" bind:this={cardErrors} role='alert' />
-    </div>
-
-    <!-- end of stripe stuff -->
-
-    <!-- error message -->
-    {#if isEmpty}
-    
-    <p class='form-empty'>please fill out the name field</p>
-    
-    {/if}
-    <!-- submit button -->
-    <button type='submit' 
-    class='btn-btn-block btn-primary'
-    disabled={isEmpty} 
-    class:disabled={isEmpty}>submit</button>
-
-
-    </form>
-</section>
+			<!-- error message -->
+			{#if isEmpty}
+				<p class="form-empty">please fill out the name field</p>
+			{/if}
+			<!-- submit button -->
+			<button type="submit" class="btn-btn-block btn-primary" disabled={isEmpty} class:disabled={isEmpty}
+				>submit</button
+			>
+		</form>
+	</section>
 {:else}
-<div class="checkout-empty">
-<h2>your cart is empty</h2>
-<a href="/products" use:link class='btn btn-primary'>fill it</a>
-</div>
+	<div class="checkout-empty">
+		<h2>your cart is empty</h2>
+		<a href="/products" use:link class="btn btn-primary">fill it</a>
+	</div>
 {/if}
