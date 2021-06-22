aba27190e8b80b98b9dd720a97feb4989be71f86
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-06-21 21:49:02 -0400

    more image work

--- src/strapi/getProducts.js
+++ src/strapi/getProducts.js
@@ -1,7 +1,7 @@
 import url from './URL';
 
 export default async () => {
-    const response = await fetch(`${url}/products`).catch (error => console.error(error));
+    const response = await fetch(`${url}/products`).catch (error => console.log(error));
     const products = await response.json();
     if (products.error) {
         return null;
