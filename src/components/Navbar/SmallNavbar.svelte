673bb215c4aa6354451d7ea365cf94cbdbfc8ddb
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-06-23 22:03:14 -0400

    url work

--- src/components/Navbar/SmallNavbar.svelte
+++ src/components/Navbar/SmallNavbar.svelte
@@ -17,7 +17,7 @@
        </button>
        <!-- logo -->
        <a href="/" use:link class='nav-logo'> 
-    <img src="/assets/images/logo.png" class='logo' alt="anime-warelogo">
+    <img src="/assets/images/logo.png" class='logo' alt="anime-warehouse logo">
     </a>
     <!-- cart button -->
    <CartButton />
--- src/stores/defaultProducts.js
+++ src/stores/defaultProducts.js
@@ -1,5 +1,8 @@
 import { writable, derived } from "svelte/store";
+import getProducts from "../strapi/getProducts";
 import localProducts from "../localProducts";
+
+
 const store = writable(flattenProducts([...localProducts]));
 
 // subscribe
@@ -13,6 +16,18 @@ function flattenProducts(data) {
     return { ...item, image };
   });
 }
+
+
+async function setProducts() {
+  let products = await getProducts();
+  if (products) {
+    products = flattenProducts(products);
+    store.set(products);
+  }
+}
+
+
+
 // featured store
 export const featuredStore = derived(store, $featured => {
   return $featured.filter(item => item.featured === true);
