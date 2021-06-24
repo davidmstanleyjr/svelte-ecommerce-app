322baf14335a7d8547f8301960b6d81daeb9e79e
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-06-24 18:43:24 -0400

    url

--- src/strapi/URL.js
+++ src/strapi/URL.js
@@ -1,3 +1,3 @@
 // url for deployed project. Makes life easier so i'm not changing things around back and forth. To run locally, use http://localhost:1337
-export default "https://davids-svelte-store-server.herokuapp.com";
+export default "https://davids-svelte-store-server.herokuapp.com && CLOUDINARY_URL=cloudinary://433879172632495:6PiV1ALX00nkL31NdWBF6ycPVNg@ddet8to42";
 //  export default "http://localhost:1337";
